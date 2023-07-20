5b0db9c1cd8d36904826c04fa8ec2481eb7f11d6
Aditya Ranjan <108622072+Crazy-Coder07@users.noreply.github.com>
2023-07-20 18:51:25 +0530

    code for production

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const colors = require("colors");
 const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path=require("path")
 //dot config
 dotenv.config();
 
@@ -25,6 +26,14 @@ app.use("/api/v1/auth", require("./routes/authRoutes"));
 app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
 app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
+
+// STATIC FOLDER
+app.use(express.static(path.join(__dirname,"./client/build")));
+
+// STATIC ROUTES
+app.get("*",function(req,res){
+  res.sendFile(path.join(__dirname,"./client/build/index.html"));
+})
 
 //port
 const PORT = process.env.PORT || 8080;
